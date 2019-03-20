b593460a564e8a0bd1c27143f9fa1f05c37b5dc7
phuhao00 <hhaou@outlook.com>
2019-03-20 23:50:10 +0800

    add show all user service

--- user.sql
+++ user.sql
@@ -1,9 +1,11 @@
+-- auto-generated definition
 create table user
 (
   id        int auto_increment
     primary key,
   user_name varchar(100) null,
   password  varchar(100) null,
-  age       int          null
+  age       int          null,
+  phone     varchar(20)  null
 );
 
